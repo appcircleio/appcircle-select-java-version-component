b3e76edc276870fc01a7ec9730dd18ace044e3e5
ftmzhrsckldr <ftmzhrsckldr@protonmail.com>
2024-11-26 11:08:56 +0300

    Logs improved

--- main.rb
+++ main.rb
@@ -17,17 +17,29 @@ def env_has_key(key)
 end
 
 def run_command(command)
-	puts "@@[command] #{command}"
-	stdout_str, stderr_str, status = Open3.capture3(command)
+  stdout_str, stderr_str, status = Open3.capture3(command)
+  if status.success?
+    return stderr_str.empty? ? stdout_str : stderr_str
+  else
+    raise stderr_str
+  end
+end
 
-	if status.success?
-		puts stdout_str unless stdout_str.empty?
-		puts stderr_str unless stderr_str.empty?
-		return stderr_str.empty? ? stdout_str : stderr_str
-	else
-		puts "@@[error] Command failed:\n#{stderr_str}"
-		raise stderr_str
-	end
+def run_command_with_log(command)
+  puts "@@[command] #{command}"
+  begin
+    output = run_command(command)
+    puts output unless output.empty?
+    output
+  rescue StandardError => e
+    puts "@@[error] Command failed:\n#{e.message}"
+    raise e
+  end
+end
+
+def get_java_version()
+	output = run_command('javac -version')
+	output.match(/javac (\d+)\.\d+\.\d+/)[1]
 end
 
 def abort_with1(message)
@@ -48,27 +60,26 @@ end
 
 sdkman_dir = env_has_key('SDKMAN_DIR')
 ac_selected_java_version = env_has_key('AC_SELECTED_JAVA_VERSION')
-puts "Selected Java version: #{ac_selected_java_version}"
+puts "Selected Java version: #{ac_selected_java_version.blue}"
 
 selected_java_version = get_env_variable("JAVA_HOME_#{ac_selected_java_version}_X64")
 if !selected_java_version
   abort_with1("Java version #{selected_java_version} is not available on the runner. Available Java versions: #{get_available_java_versions()}.")
 end
 
-current_java_version = run_command('javac -version').match(/javac (\d+)\.\d+\.\d+/)[1]
-puts "Current Java Version: #{current_java_version}"
+current_java_version = get_java_version()
+puts "Current Java Version: #{current_java_version.blue}"
 
 if ac_selected_java_version == current_java_version
 	puts "Current Java version is already the same as the selected Java version: #{ac_selected_java_version}.".yellow
   exit 0
 end
 
-puts "Changing default Java version from #{current_java_version} to selected #{ac_selected_java_version}."
+puts "Changing default Java version from #{current_java_version.blue} to selected #{ac_selected_java_version.blue}."
 open(ENV['AC_ENV_FILE_PATH'], 'a') { |f|
 	f.puts "JAVA_HOME=#{selected_java_version}"
 }
 
-run_command("bash -l -c \"source '#{sdkman_dir}/bin/sdkman-init.sh' && sdk default java $(basename #{selected_java_version})\"")
-run_command("javac -version")
+run_command_with_log("bash -l -c \"source '#{sdkman_dir}/bin/sdkman-init.sh' && sdk default java $(basename #{selected_java_version})\"")
 
-get_info_msg("New Java version (JAVA_HOME) path: #{selected_java_version}.")
+get_info_msg("New Java version: #{get_java_version()}")
